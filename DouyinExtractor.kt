89a4a0b3d92091ba7a1a1d5ae3b50e9f56c3c871
TinyZ <zht1246@outlook.com>
2026-04-16 14:47:09 +0800

    Fix Kotlin syntax by removing unnecessary semicolons in DouyinExtractor.kt

---
+++ DouyinExtractor.kt
@@ -0,0 +1,12 @@
+// DouyinExtractor.kt
+
+class DouyinExtractor {
+
+    fun extract(url: String) {
+        // Extraction logic goes here
+    }
+
+    private fun someOtherFunction() {
+        // Some logic
+    }
+}
